9b69e7d69fdba4f2c1618c15f7831873bf14a8c7
mybhadre <56539063+mybhadre@users.noreply.github.com>
2024-02-27 14:26:23 +0800

    Update create_repo.py

--- create_repo.py
+++ create_repo.py
@@ -19,8 +19,8 @@ reponame = "mybhadre/test-oic"
 #repo = user.create_repo(new_repo)
 #print(f"Repository '{new_repo}' created successfully!")
 reponame = "mybhadre/demo-oic"
-#token = "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"
-token = os.getenv('GH_TOKEN')
+token = "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"
+#token = os.getenv('GH_TOKEN')
 g = Github(token)
 user = g.get_user()
 print(user)
@@ -30,5 +30,5 @@ data = {"name": "{}".format(reponame)}
 print(headers)
 print(data)
 
-r = requests.post(GITHUB_API_URL + 'user/repos', data=json.dumps(data), headers=headers)
+r = requests.post(GITHUB_API_URL + 'user/repos', data=json.dumps(data), auth=(mybhadre,token))
 print(r)
